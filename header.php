17570e3c2fe007b665d0d39ef3c49e3dba51fee0
Mr-EEEE1 <163358285+Mr-EEEE1@users.noreply.github.com>
2025-02-26 13:14:57 +1300

    Update header.php

--- header.php
+++ header.php
@@ -1,1 +1,6 @@
-
+ <header>
+        <figure>
+            <img src="images/lc-mint.png" width="100" alt="Learn Coach Logo.">
+        </figure>
+        <h1>Digital Media. Advanced Websites</h1>
+    </header>
