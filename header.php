02fac7058dfb324d62f16ad8185fca64da7c3ed8
Mr-EEEE1 <163358285+Mr-EEEE1@users.noreply.github.com>
2025-03-19 12:33:56 +1300

    Update header.php

--- header.php
+++ header.php
@@ -2,5 +2,5 @@
         <figure>
             <img src="images/lc-mint.png" width="100" alt="Learn Coach Logo.">
         </figure>
-        <h1>Radnom Website... Once Again</h1>
+        <h1>Random Website... Once Again</h1>
     </header>
